e8e16adc53febc636b49cfb4f2a0f72f48cd2004
Bruno Simioni <brunosimioni@gmail.com>
2018-04-20 15:56:16 -0300

    first version

--- server.js
+++ server.js
@@ -1,15 +1,36 @@
-var port = process.env.PORT || 8080;
+var schedule = require('node-schedule');
+var unirest = require('unirest');
+var hangoutsChatRoom = "https://chat.googleapis.com/v1/spaces/AAAAGepSZ08/messages?key=AIzaSyDdI0hCZtE6vySjMm-WEfRq3CPzqKqqsHI&token=lE5A54ZWlDlcwvQD1YAg_gzWO81Z5Kl2uoN9mUXeA4g%3D";
 
-var express = require('express');
-var bodyParser = require('body-parser');
+var j = schedule.scheduleJob('* * * * *', function(){
+  checkAlert();
+});
 
-var app = express();
+function checkAlert() {
+  console.log("checking alerts...");
+  unirest.get('http://www.rodoaneloeste.com.br/generic/home/ListOccurrences?abc&_=1524247552871')
+  .headers({'Accept': '*/*', 'Accept-Encoding': 'gzip, deflate'})
+  .end(function (response) {
+    console.log("analysing alerts...");
+    checkProblems(response.body);
+  });
+}
 
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
-
-app.use(express.static(__dirname + '/public'));
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-
-app.listen(port);
+function checkProblems(occurences) {
+  var events = occurences.Occurences;
+  for (var i in events) {
+    var e = events[i];
+    console.log("analysing event...");
+    if (e.OccurrenceTypeCssClass != 'green')
+    {
+      console.log("posting problem...");
+      console.log(e);
+      unirest.post(hangoutsChatRoom)
+      .headers({'Content-Type': 'application/json'})
+      .send({ "text": e.Text})
+      .end(function (response) {
+        console.log(response);
+      });
+    }
+  }
+}
